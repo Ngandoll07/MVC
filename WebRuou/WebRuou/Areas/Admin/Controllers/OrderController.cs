67cc7dc8c2aa91f7fd349f72d9045c642aa7abb4
Ngandoll <22dh112324@st.huflit.edu.vn>
2025-03-11 14:17:22 +0700

    update coupon

---
+++ WebRuou/WebRuou/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebRuou.Models;
+
+namespace WebRuou.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        DBRuouEntities db = new DBRuouEntities();
+        // GET: Admin/Order
+        public ActionResult Index( int? page)
+        {
+            int pageSize = 10;
+            int pageNum = (page ?? 1);
+
+            var orders = db.Orders.ToList().ToPagedList(pageNum, pageSize);
+            return View(orders);
+        }
+    }
+}
